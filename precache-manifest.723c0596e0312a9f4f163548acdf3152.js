83e66698ab44481f9183ed864606c66fef7b3feb
Rahul Shrivastava <rahul3103@gmail.com>
2019-04-12 17:57:44 +0530

    Updates

---
+++ precache-manifest.723c0596e0312a9f4f163548acdf3152.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7c8d063ca96f81c35c80",
+    "url": "/live-sports-news/static/js/runtime~main.a64c0e45.js"
+  },
+  {
+    "revision": "4cde63c7dbf75abc9517",
+    "url": "/live-sports-news/static/js/main.f823144f.chunk.js"
+  },
+  {
+    "revision": "74e2e2c4f866953528f4",
+    "url": "/live-sports-news/static/js/2.c33365c1.chunk.js"
+  },
+  {
+    "revision": "4cde63c7dbf75abc9517",
+    "url": "/live-sports-news/static/css/main.b37e8c97.chunk.css"
+  },
+  {
+    "revision": "fe3ff01fe43ef8f550282f2e5bdd7ffa",
+    "url": "/live-sports-news/index.html"
+  }
+];
